b58a32e84878e6a3dcc5450ba9d7d90198f96c92
LilPrice-Code <littleprice002@gmail.com>
2019-12-18 16:26:41 -0600

    editing  boardgame

--- DDGameProject/DDGameApp/static/DDGameApp/js/gameboard.js
+++ DDGameProject/DDGameApp/static/DDGameApp/js/gameboard.js
@@ -1,13 +1,23 @@
 console.log("game on");
-
+var x = 0;
+var y = 0;
+var gap = 25;
 function drawline() {
-
+    var canvas = document.getElementById("mycanvas");
+    var ctx = canvas.getContext("2d");
+    while (x <= 1000){
+        ctx.moveTo(x, 0);
+        ctx.lineTo(x, 500);
+        ctx.stroke();
+        x = x + gap;
+    }
+    while (y <= 500){
+        ctx.moveTo(0, y);
+        ctx.lineTo(1000, y);
+        ctx.stroke();
+        y = y + gap;
+    }
 }
-var canvas = document.getElementById("mycanvas");
-var ctx = canvas.getContext("2d");
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 100);
-ctx.stroke();
 
 // var canvas = document.getElementById("mycanvas");
 // var ctx = canvas.getContext("2d");
